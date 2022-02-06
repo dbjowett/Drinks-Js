3f08310297269ce4b52d65e55025d01c5b2ecc05
Daniel Jowett <dbjowett@gmail.com>
2022-02-06 20:17:48 +0900

    Add custom styling, remove Chakra

--- components/Cocktail_Item.js
+++ components/Cocktail_Item.js
@@ -1,18 +1,22 @@
 import { GridItem, Button, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/component/Cocktail_Item.module.css';
 
 export default function CocktailItem({ id, title, description }) {
   return (
     <div className={styles.container}>
-      <Text textSize='5xl'>{title}</Text>
+      <Image src='/../public/images/WhiskeySour.png' width='300' height='300' alt='' />
+      <Text fontSize='2xl'>{title}</Text>
       <div>{description}</div>
-      <Link href={`/cocktail/${id}`} passHref>
-        <button> Learn More </button>
-      </Link>
-      <Link href={`/cocktail/edit/${id}`} passHref>
-        <button>Edit</button>
-      </Link>
+      <div className={styles.btn_container}>
+        <Link href={`/cocktail/${id}`} passHref>
+          <button> Learn More </button>
+        </Link>
+        {/* <Link href={`/cocktail/edit/${id}`} passHref>
+          <button>Edit</button>
+        </Link> */}
+      </div>
     </div>
   );
 }
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,12 +4,12 @@ import { VStack } from '@chakra-ui/react';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div>
       <Head>
         <title>Cocktails</title>
       </Head>
       <NavBar />
       <VStack spacing='30px'>{children}</VStack>
-    </>
+    </div>
   );
 }
